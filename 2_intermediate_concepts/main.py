6681795b838b4f75fabf9540e743607b3cf75b94
atharva-suryawanshi21 <s.atharva2003@gmail.com>
2023-12-21 19:10:27 +0530

    .

--- 2_intermediate_concepts/main.py
+++ 2_intermediate_concepts/main.py
@@ -3,6 +3,7 @@ import schema
 import model_schema
 from database_connection import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
 
@@ -65,24 +66,73 @@ def get_all_blogs(db: Session = Depends(get_db)):
 @app.get('/blog/{id}')
 # to get specific blog
 # also handle situation where the requested id is absent
-def get_specific(id: int,  get_response: Response, db: Session = Depends(get_db)):
+def get_specific(id: int, db: Session = Depends(get_db)):
     blog = db.query(model_schema.Blog).filter(
         model_schema.Blog.id == id).first()
 
     if not blog:
         # get_response.status_code = status.HTTP_404_NOT_FOUND
         # return {'Detail': f"The blog with id : {id} not found"}
+        # for above code add 'get_response:Response' in the parameter of get_specific
         # instead of above code, below code to implemented
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"The blog with id : {id} not found")
     return blog
 
 
-@app.delete('/blog/{id}')
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 # to delete a blog
 def delete(id, db: Session = Depends(get_db)):
-    db.query(model_schema.Blog).filter(
-        model_schema.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(model_schema.Blog).filter(
+        model_schema.Blog.id == id)
 
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"the blog with id:{id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
     return {'detail': f"The blog with id:{id}, is deleted"}
+# synchronize_session=False: if any instances of deleted object are present in the session
+#                            , they won't be automatically removed or updated
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+# to update a blog
+def update(id, request: schema.blog_info, db: Session = Depends(get_db)):
+    blog = db.query(model_schema.Blog).filter(
+        model_schema.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"blog with id:{id} not found")
+    blog.update(request.model_dump())
+    db.commit()
+    return "updated successfully"
+# .update() take dict as parameters so we need to convert our pydantic model(blog_info) to dict
+
+# ______________________________________________________________________________
+# Response model
+# what if we need data of only titles? We create a new schema, where only title is defined
+# using this new model, we input it to decorator
+
+
+@app.get('/blog_title/{id}', response_model=schema.blog_title)
+# to get one blog title
+def get_title(id: int, db: Session = Depends(get_db)):
+    blog = db.query(model_schema.Blog).filter(
+        model_schema.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"The blog with id : {id} not found")
+    return blog
+# we only get title of blog as the class blog_title has only title attribute
+
+
+@app.get('/blog_title', response_model=List[schema.blog_title])
+# to get titles of all blogs
+def get_all_titles(db: Session = Depends(get_db)):
+    blog = db.query(model_schema.Blog).all()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"empty")
+    return blog
+# the List[] notaion indicates that the endpoint is expected to return a list of items
--- 2_intermediate_concepts/schema.py
+++ 2_intermediate_concepts/schema.py
@@ -4,5 +4,18 @@ from typing import Optional
 
 class blog_info(BaseModel):
     title: str
-    title: str
     body: str
+
+
+class blog_title(BaseModel):
+    title: str
+
+    class Config():
+        from_attributes = True
+
+# from_attributes = True: Pydantic models are capable of:
+#                   1. automatically converts ORM models to Pydantic models
+#                   2. enabling ORM-specific behaviour such as allowing nested models,
+#                      ignoring non-ORM-related attributes, etc
+# Config: allows to configure various settings for the model, like how data is parsed
+#         how validation occurs and other behavioral aspects of the model
