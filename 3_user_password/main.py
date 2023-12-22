addd9a1eb8dc7029212a76cba9e332241ae92ed0
atharva-suryawanshi21 <s.atharva2003@gmail.com>
2023-12-22 09:17:54 +0530

    .

---
+++ 3_user_password/main.py
@@ -0,0 +1,7 @@
+# Now you can import module1.py
+
+
+from database_connection import engine
+import sys
+# Add the path to the directory containing module1.py
+sys.path.append('2_intermediate_concepts')
